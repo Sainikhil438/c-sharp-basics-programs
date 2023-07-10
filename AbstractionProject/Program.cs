c49023452b886f6e43be5079a6738a6de3a26b35
Sainikhil438 <sainikhil392@gmail.com>
2023-07-10 07:10:43 +0530

    [Sainikhil] add:abstraction file

---
+++ AbstractionProject/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AbstractionProject
+{
+    abstract class Parent
+    {
+        public abstract void P1();
+        public void P2()
+        {
+            Console.WriteLine("normal method");
+        }
+    }
+    class Child: Parent
+    {
+        public override void P1()
+        {
+            Console.WriteLine("Overrided method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child cc = new Child();
+            cc.P1();
+            cc.P2();
+            Console.ReadLine();
+        }
+    }
+}
