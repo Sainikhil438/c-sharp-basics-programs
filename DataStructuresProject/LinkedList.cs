8973fd6410407b81223104610b607fe2d65c1e4b
Sainikhil438 <sainikhil392@gmail.com>
2023-07-10 08:34:48 +0530

    [Sainikhil] add:datastructures file

---
+++ DataStructuresProject/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresProject
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data+" inserted into Linked list");
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node RemoveFirstNode()
+        {
+            if(this.head==null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        internal Node RemoveLastNode()
+        {
+            if(head==null)
+            {
+                return null;
+            }
+            if(head.next==null)
+            {
+                return null;
+            }
+            Node newNode = head;
+            while(newNode.next.next!=null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+
+    }
+}
---
+++ DataStructuresProject/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructuresProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LinkedList ls = new LinkedList();
+            ls.Add(55);
+            ls.Add(35);
+            ls.Add(75);
+            ls.Display();
+
+            //To remove First Node from linked list
+            ls.RemoveFirstNode();
+            ls.Display();
+
+            //To remove Last Node from Linked List
+            ls.RemoveLastNode();
+            ls.Display();
+        }
+    }
+}
